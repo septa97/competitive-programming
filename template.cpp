77f2560772d2ab4e83839f7b84c68d2906f222cf
septa97 <jancharles.adona@gmail.com>
2018-02-25 20:55:31 +0800

    Add macro for scanning unsigned integral data types

--- template.cpp
+++ template.cpp
@@ -11,6 +11,7 @@
 #define MOD 1000000009
 #define debug(x) cout << x << " ";
 #define debug_newline(x) cout << x << endl;
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0) // for unsigned integral data types
 
 using namespace std;
 
