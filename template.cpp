8b18e0c903157a4f90c26b711a94eb16e2f02080
septa97 <jancharles.adona@gmail.com>
2018-02-25 18:27:16 +0800

    Add C++ template

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <cstdio>
+#include <cstring>
+
+#define MOD 1000000009
+#define debug(x) cout << x << " ";
+#define debug_newline(x) cout << x << endl;
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> p_ii;
+
+int main() {
+	return 0;
+}
