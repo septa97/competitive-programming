2d7ab087bb40269a54238d171c1a56442b4864bf
septa97 <jancharles.adona@gmail.com>
2018-03-01 20:45:20 +0800

    Simple absolute value. Be careful of overflow

---
+++ Kattis/different.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <utility>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <cstdio>
+#include <cstring>
+
+#define MOD 1000000009
+#define debug(x) cout << x << " ";
+#define debug_newline(x) cout << x << endl;
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0) // for unsigned integral data types
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> p_ii;
+
+int main() {
+    ll a, b;
+    
+    while(cin >> a >> b) {
+        cout << (abs(a-b)) << endl;
+    }
+
+    return 0;
+}
