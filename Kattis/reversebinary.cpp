9b4ec508607b976ed93dd0d1187b1ef1c05b9abb
septa97 <jancharles.adona@gmail.com>
2018-03-06 20:14:37 +0800

    Bit shifting

---
+++ Kattis/reversebinary.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <utility>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+
+#define MOD 1000000009
+#define debug(x) cout << x << " ";
+#define debug_newline(x) cout << x << endl;
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0) // for unsigned integral data types
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> p_ii;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int ans = 0;
+
+    while (n > 0) {
+        ans <<= 1;
+        ans |= n&1;
+        n >>= 1;
+    }
+
+    printf("%d", ans);
+
+    return 0;
+}
