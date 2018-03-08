a805a31050440b6e5185be68d607e23c2dee36d5
septa97 <jancharles.adona@gmail.com>
2018-03-08 09:14:27 +0800

    Add helper function for splitting a string

--- template.cpp
+++ template.cpp
@@ -12,7 +12,7 @@
 
 #define MOD 1000000009
 #define debug(x) cout << x << " ";
-#define debug_newline(x) cout << x << endl;
+#define debug_newline(x) cout << x << "\n";
 #define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0) // for unsigned integral data types
 
 using namespace std;
@@ -20,6 +20,21 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef pair<int, int> p_ii;
+
+vector<string> split(string s, string delimiter) {
+    int pos = s.find(delimiter);
+    vector<string> a;
+
+    while (pos != -1) {
+        a.push_back(s.substr(0, pos));
+        s = s.substr(pos+1);
+        pos = s.find(delimiter);
+    }
+
+    a.push_back(s);
+
+    return a;
+}
 
 int main() {
     return 0;
