b1a513facb11f82f2943524f3938e9f5fd021100
septa97 <jancharles.adona@gmail.com>
2018-03-15 20:33:49 +0800

    Kadane's algorithm

---
+++ Kattis/commercials.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <utility>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+
+#define MOD 1000000009
+#define INF 1000000000
+#define debug(x) cout << x << " ";
+#define debug_newline(x) cout << x << "\n";
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0) // for unsigned integral data types
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> p_ii;
+
+vector<string> split(string s, string delimiter) {
+    int pos = s.find(delimiter);
+    vector<string> a;
+
+    while (pos != -1) {
+        a.push_back(s.substr(0, pos));
+        s = s.substr(pos+1);
+        pos = s.find(delimiter);
+    }
+
+    a.push_back(s);
+
+    return a;
+}
+
+int A[100005];
+
+int main() {
+    int n, p;
+    cin >> n >> p;
+
+    for (int i = 0; i < n; i++) cin >> A[i];
+
+    int ans = 0, curr = 0;
+    for (int i = 0; i < n; i++) {
+        curr = max(0, curr+A[i]-p);
+        ans = max(curr, ans);
+    }
+    
+    cout << ans;
+
+    return 0;
+}
