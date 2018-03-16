e7c376a13ef984f7c7eba8efe180aabfe415cf59
septa97 <jancharles.adona@gmail.com>
2018-03-16 21:55:23 +0800

    Logarithm

---
+++ Kattis/3dprinter.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <utility>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+
+#define MOD 1000000007
+#define INF 1000000000
+#define debug(x) cout << x << " ";
+#define debug_newline(x) cout << x << "\n";
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0) // for unsigned integral data types
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> p_ii;
+
+vector<string> split(string s, string delimiter) {
+    int pos = s.find(delimiter);
+    vector<string> a;
+
+    while (pos != -1) {
+        a.push_back(s.substr(0, pos));
+        s = s.substr(pos+1);
+        pos = s.find(delimiter);
+    }
+
+    a.push_back(s);
+
+    return a;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int temp = (int)ceil(log2(n));
+    n = 1 << (temp+1);
+
+    cout << (int)log2(n);
+
+    return 0;
+}
